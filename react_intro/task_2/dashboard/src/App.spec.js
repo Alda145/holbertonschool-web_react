f5678cc912f48358467d2e9c4dd8dd0021575c8d
Alda145 <limiesmeralda@yahoo.com>
2026-03-04 16:55:24 +0100

    Changes

--- react_intro/task_2/dashboard/src/App.spec.js
+++ react_intro/task_2/dashboard/src/App.spec.js
@@ -1,7 +1,21 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
+test("renders h1 with School Dashboard text", () => {
+    render(<App />)
+    expect(screen.getByRole('heading')).toHaveTextContent(/School dashboard/i);
+})
 
+test("renders correct text in body and footer paragraphs", () => {
+    render(<App />)
+    expect(screen.getByText(/Login to access the full dashboard/i));
+    expect(screen.getByText(/Copyright/i));
+})
+test("renders an image", () => {
+    render(<App />);
+    const image = screen.getByAltText(/holberton logo/i);
+    expect(image).toBeInTheDocument();
+});
 test("2 labels email e password",  () => {
     render(<App/>)
     const labelEmail = screen.getByRole('label').toHaveTextContent(/Email/i);
