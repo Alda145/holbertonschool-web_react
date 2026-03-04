ade2257a705c5ac3cd15b258d61d5f1687947761
Alda145 <limiesmeralda@yahoo.com>
2026-03-04 15:14:15 +0100

    Changes

--- react_intro/task_0/dashboard/src/App.spec.js
+++ react_intro/task_0/dashboard/src/App.spec.js
@@ -4,13 +4,13 @@ import App from "./App";
 
 test("renders h1 with School Dashboard text",  () => {
     render(<App/>)
-    expect(screen.getByRole('heading')).toHaveTextContent('School dashboard');
+    expect(screen.getByRole('heading')).toHaveTextContent('/School dashboard/i');
 })
 
 test("renders correct text in body and footer paragraphs", () => {
     render(<App />)
-    expect(screen.getByText('Login to access the full dashboard'));
-    expect(screen.getByText("Copyright"));
+    expect(screen.getByText('/Login to access the full dashboard/i'));
+    expect(screen.getByText("/Copyright/i"));
 })
 test("renders an image", () => {
     render(<App />);
