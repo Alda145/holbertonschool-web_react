a6d67e01a37975a12f8fd2729e73f61e4b623d03
Alda145 <limiesmeralda@yahoo.com>
2026-03-04 15:45:48 +0100

    Changes

--- react_intro/task_0/dashboard/src/utils.js
+++ react_intro/task_0/dashboard/src/utils.js
@@ -2,13 +2,13 @@
 
 
 
-const getCurrentYear  = ()=>{
+export  function getCurrentYear()  {
 const date = new Date()
 return date.getFullYear();
 
 }
-getCurrentYear()
-const getFooterCopy = (isIndex)=>{
+
+export function getFooterCopy() {
 if (isIndex=== true){
     return 'Holberton School'
 }else {
@@ -16,4 +16,4 @@ if (isIndex=== true){
 }
 
 }
-getFooterCopy(false);
+
