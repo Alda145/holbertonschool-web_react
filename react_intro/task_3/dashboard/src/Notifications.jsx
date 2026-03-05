748d7f1b32bdbe0911aacd5179d5794ac052bf43
Alda145 <limiesmeralda@yahoo.com>
2026-03-05 16:50:57 +0100

    Changes

--- react_intro/task_3/dashboard/src/Notifications.jsx
+++ react_intro/task_3/dashboard/src/Notifications.jsx
@@ -9,7 +9,7 @@ const Notifications = () => {
             <ul>
                 <li data-priority="default">New course available</li>
                 <li data-priority="urgent">New resume available</li>
-                <li data-priority="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+                <li data-priority="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() || '' }}></li>
             </ul>
             <button aria-label="Close" style={{ position: 'absolute', top: '10px', right: '10px' }}
                 onClick={() => console.log('Close button has been clicked')}>
