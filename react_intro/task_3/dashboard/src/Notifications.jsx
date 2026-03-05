c6c0f7260647f4483c404d5ed72ad0391f5ae207
Alda145 <limiesmeralda@yahoo.com>
2026-03-05 16:08:31 +0100

    Changes

--- react_intro/task_3/dashboard/src/Notifications.jsx
+++ react_intro/task_3/dashboard/src/Notifications.jsx
@@ -1,23 +1,12 @@
-import './Notifications.css';
-import close from './assets/close-button.png';
-
-const Notifications = () => {
-    return (
-        <div className="notification-items">
-            <p>Here is the list of notifications</p>
-            <ul>
-                <li data-priority="default">New course available</li>
-                <li data-priority="urgent">New resume available</li>
-                <li data-priority="urgent">Urgent requirement - complete by EOD</li>
-            </ul>
-            <button
-                aria-label="Close"
-                onClick={() => console.log('Close button has been clicked')}
-            >
-                <img src={close} alt="close button" />
-            </button>
-        </div>
-    );
-};
-
-export default Notifications;
+<div className="notification-items">
+    <p>Here is the list of notifications</p>
+    <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li data-priority="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+    </ul>
+    <button aria-label="Close" style={{ position: 'absolute', top: '10px', right: '10px' }}
+        onClick={() => console.log('Close button has been clicked')}>
+        <img src={close} style={{ width: '30px', height: '30px' }} />
+    </button>
+</div>
