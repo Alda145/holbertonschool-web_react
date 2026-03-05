1c5465cc78576b27c22d81104371509e98ae8681
Alda145 <limiesmeralda@yahoo.com>
2026-03-05 15:14:10 +0100

    Changes

--- react_intro/task_3/dashboard/src/Notifications.spec.js
+++ react_intro/task_3/dashboard/src/Notifications.spec.js
@@ -30,7 +30,7 @@ describe('Notification component', () => {
         fireEvent.click(button);
 
         expect(consoleSpy).toHaveBeenCalledWith(
-            'Close button has been clicked.'
+            'Close button has been clicked'
         );
 
         consoleSpy.mockRestore();
