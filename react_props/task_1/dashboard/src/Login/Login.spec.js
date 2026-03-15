81465b32057521eb020af80a2ae6600ecff350a8
Alda145 <limiesmeralda@yahoo.com>
2026-03-15 13:42:20 +0100

    Changes apply

--- react_props/task_1/dashboard/src/Login/Login.spec.js
+++ react_props/task_1/dashboard/src/Login/Login.spec.js
@@ -1,21 +1,21 @@
+import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import Login from "./Login";
 
 describe("Login component", () => {
-
-    test("renders 2 labels, 2 inputs and 1 button", () => {
+    it("renders 2 labels, 2 inputs and 1 button", () => {
         const { container } = render(<Login />);
 
         const labels = container.querySelectorAll("label");
         const inputs = container.querySelectorAll("input");
-        const button = container.querySelector("button");
+        const button = container.querySelectorAll("button");
 
         expect(labels.length).toBe(2);
         expect(inputs.length).toBe(2);
-        expect(button).not.toBeNull();
+        expect(button.length).toBe(1);
     });
 
-    test("inputs get focus when label is clicked", () => {
+    it("focuses input when label is clicked", () => {
         const { container } = render(<Login />);
 
         const emailLabel = container.querySelector('label[for="email"]');
@@ -25,5 +25,4 @@ describe("Login component", () => {
 
         expect(document.activeElement).toBe(emailInput);
     });
-
 });
