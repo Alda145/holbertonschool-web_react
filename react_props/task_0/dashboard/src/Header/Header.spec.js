97594a3b631165d9de6712bbd06669938da51bfc
Alda145 <limiesmeralda@yahoo.com>
2026-03-15 13:19:01 +0100

    Changes apply

---
+++ react_props/task_0/dashboard/src/Header/Header.spec.js
@@ -0,0 +1,25 @@
+
+import { render, screen } from "@testing-library/react";
+import Header from "./Header";
+
+describe("Header component", () => {
+
+    test("renders Holberton logo", () => {
+        render(<Header />);
+
+        const logo = screen.getByAltText("holberton logo");
+        expect(logo).toBeInTheDocument();
+    });
+
+    test("renders h1 with correct text", () => {
+        render(<Header />);
+
+        const heading = screen.getByRole("heading", {
+            level: 1,
+            name: /school dashboard/i
+        });
+
+        expect(heading).toBeInTheDocument();
+    });
+
+});
